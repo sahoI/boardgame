0cb0e46dd4031038cd46e45d4c60a603a3f830bb
sahoI <saho0003@gmail.com>
2018-09-27 22:23:28 +0900

    ファイルを分け、クリックによって移動するようになった。

---
+++ initialize.py
@@ -0,0 +1,122 @@
+import pygame
+from pygame.locals import *
+import sys
+
+
+def rects(screen, x, y, color):  # 各色のこま
+    for num in range(10):
+        pygame.draw.rect(screen, color, (x[num] - 10, y[num] - 10, 20, 20))
+    font(screen, x, y)
+
+
+def draw_line(screen):  # 盤の目
+    x = 440
+    y = 80
+    for count in range(9):
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (x, 520 - y), 1)  # 右から左
+        pygame.draw.line(screen, (0, 0, 0), (x + 80, 320 - y), (x + 80, 200 + y), 1)  # 左から右
+        pygame.draw.line(screen, (0, 0, 0), (x + 120, y + 180), (200, y * 2 - 80), 1)  # 右上から左下
+        pygame.draw.line(screen, (0, 0, 0), (440, 560 - y * 2), (x - 40, 500 - y), 1)  # 右下から左上
+        pygame.draw.line(screen, (0, 0, 0), (x - 40, y + 20), (440, y * 2 - 40), 1)  # 右上から左下
+        pygame.draw.line(screen, (0, 0, 0), (x + 120, 340 - y), (200, 600 - y * 2), 1)  # 右下から左上
+        x -= 40
+        y += 20
+
+
+def font(screen, x, y):
+    num = [1]
+    for i in range(9):
+        num.append(i + 2)
+    sysfont = pygame.font.SysFont(None, 20)
+    for i in range(10):
+        text = sysfont.render(str(num[i]), True, (255, 255, 255))
+        screen.blit(text, (x[i] - 5, y[i] - 5))
+
+# def record(screen):
+
+
+def background(screen):
+    x = 0
+    for count in range(120):
+        pygame.draw.line(screen, (0, 255, 0), (x, 0), (x, 800), 2)  # 右上から左下
+        pygame.draw.line(screen, (0, 255, 0), (0, x), (1200, x), 2)  # 右上から左下
+        x += 10
+
+    # x座標のメモリ
+    sysfont = pygame.font.SysFont(None, 20)
+    value = 0
+    for num in range(620):
+        value+=20
+        if num % 40 == 0:
+            text = sysfont.render(str(num), True, (0, 0, 0))
+            screen.blit(text, (num, 40))
+
+
+def direction(screen): #方向キー
+    pygame.draw.rect(screen, (255,0,0), (580, 140, 40, 20), 0) #右上
+    pygame.draw.rect(screen, (255,0,0), (580, 180, 40, 20), 0) #右下
+    pygame.draw.rect(screen, (255,0,0), (500, 140, 40, 20), 0) #左上
+    pygame.draw.rect(screen, (255,0,0), (500, 180, 40, 20), 0) #左下
+    pygame.draw.rect(screen, (255,0,0), (550, 100, 20, 40), 0) #上
+    pygame.draw.rect(screen, (255,0,0), (550, 200, 20, 40), 0) #下
+
+    sysfont = pygame.font.SysFont(None, 20)
+    text = sysfont.render("R&U", True, (255, 255, 255))
+    screen.blit(text, (580, 145))
+    text = sysfont.render("R&D", True, (255, 255, 255))
+    screen.blit(text, (580, 185))
+    text = sysfont.render("L&U", True, (255, 255, 255))
+    screen.blit(text, (500, 145))
+    text = sysfont.render("L&D", True, (255, 255, 255))
+    screen.blit(text, (500, 185))
+    text = sysfont.render("UP", True, (255, 255, 255))
+    screen.blit(text, (550, 105))
+    text = sysfont.render("D", True, (255, 255, 255))
+    screen.blit(text, (550, 205))
+
+def mouseDownActionDirection(af_x, af_y,x, y,type): #遷移先のx,y座標を求める
+    if (type == "typeX"):
+        print("x")
+        if (af_x > 580)and (af_x < 620):
+            if(af_y > 140) and (af_y < 160):
+                print("R&U")
+            elif(af_y > 180) and (af_y < 220):
+                print("R&D")
+            x += 20
+        elif (af_x > 550) and (af_x < 570):
+            if (af_y > 100) and (af_y < 140):
+                print("UP")
+            elif (af_y > 200) and (af_y < 240):
+                print("DOWN")
+            x -= 20
+        elif (af_x > 500) and (af_x < 540):
+            if(af_y > 140) and (af_y < 160):
+                print("L&U")
+            elif(af_y > 180) and (af_y < 220):
+                print("L&D")
+            x -= 60
+        return x
+    else:
+        print("y")
+        if (af_x > 580)and (af_x < 620):
+            if(af_y > 140) and (af_y < 160):
+                print("R&U")
+                y -= 20
+            elif(af_y > 180) and (af_y < 220):
+                print("R&D")
+                y += 20
+        elif (af_x > 550) and (af_x < 570):
+            if (af_y > 100) and (af_y < 140):
+                print("UP")
+                y -= 40
+            elif (af_y > 200) and (af_y < 240):
+                print("DOWN")
+                y += 40
+        elif (af_x > 500) and (af_x < 540):
+            if(af_y > 140) and (af_y < 160):
+                print("L&U")
+                y -= 20
+            elif(af_y > 180) and (af_y < 220):
+                print("L&D")
+                y += 20
+        return y
--- main.py
+++ main.py
@@ -1,134 +1,105 @@
 import pygame
+import initialize
 from pygame.locals import *
 import sys
 
-def circle_position(screen, num, str): #選択する四角の作成→交点
+
+def rect_position(screen, num, str):  # 選択する四角の作成→交点
     # num1 = [2,3,10,9,8,7,8,9,10,3,2,1]
     if str == "x":
         x = 510
-        black_x=[550]
+        black_x = [550]
         for i in range(12):
             for j in range(num[i]):
                 black_x.append(x)
-            x-=40
+            x -= 40
         return black_x
 
     else:
-        black_y=[250]
-        y = [230,210,70,90,110,130,110,90,70,210,230,250]
+        black_y = [250]
+        y = [230, 210, 70, 90, 110, 130, 110, 90, 70, 210, 230, 250]
         for i in range(12):
             for j in range(num[i]):
                 black_y.append(y[i])
-                y[i]+=40
+                y[i] += 40
         return black_y
 
-#文字の色を引数にしてコマの出力を一つのメソッドで書けそう
-#x,y座標の順番を一定にすればfor文で配列を作れそうor引数にx,y座標の配列を挿入する
 
-def circles(screen, x, y, color): #各色のこま
-    for num in range(10):
-        pygame.draw.rect(screen,color,(x[num]-10,y[num]-10,20,20))
-    font(screen,x,y)
-
-def draw_line(screen): #盤の目
-    x = 440
-    y = 80
-    for count in range(9):
-        pygame.draw.line(screen, (0,0,0), (x,y), (x, 520-y), 1) #右から左
-        pygame.draw.line(screen, (0,0,0), (x+80,320-y), (x+80, 200+y), 1) #左から右
-        pygame.draw.line(screen, (0,0,0), (x+120,y+180), (200, y*2-80), 1) #右上から左下
-        pygame.draw.line(screen, (0,0,0), (440,560-y*2), (x-40, 500-y), 1) #右下から左上
-        pygame.draw.line(screen, (0,0,0), (x-40,y+20), (440, y*2-40), 1) #右上から左下
-        pygame.draw.line(screen, (0,0,0), (x+120,340-y), (200, 600-y*2), 1) #右下から左上
-        x-=40
-        y+=20
-
-def font(screen,x,y):
-    num = [1]
-    for i in range(9):
-        num.append(i+2)
-    # print(num)
-    sysfont = pygame.font.SysFont(None, 20)
-    for i in range(10):
-        text = sysfont.render(str(num[i]), True, (255,255,255))
-        screen.blit(text, (x[i]-5,y[i]-5))
-
-def background(screen):
-    x = 0
-    for count in range(120):
-        pygame.draw.line(screen, (0,255,0), (x,0), (x, 800), 2) #右上から左下
-        pygame.draw.line(screen, (0,255,0), (0,x), (1200, x), 2) #右上から左下
-        x+=40
+# 文字の色を引数にしてコマの出力を一つのメソッドで書けそう
+# x,y座標の順番を一定にすればfor文で配列を作れそうor引数にx,y座標の配列を挿入する
 
 def click_process(x, y, red_x, red_y):
     if x > 420 or x < 460 and y > 60 or y < 100:
         # pygame.draw.rect(screen,(255,255,0),(red_x[3]-10,red_y[3]-10,20,20))
         x = 430
-        y = 60
+        y = 120
     return x
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 520))
     pygame.display.set_caption("ダイヤモンドゲーム")
-    screen.fill((255,255,255))
-    background(screen) #背景
-    draw_line(screen) #盤の目
-    num = [2,3,10,9,8,7,8,9,10,3,2,1]
-    for count in range(73): #交点の円
-        pygame.draw.rect(screen,(255,255,255),(circle_position(screen, num, "x")[count],circle_position(screen, num, "y")[count],20,20))
-        pygame.draw.rect(screen,(0,0,0),(circle_position(screen, num, "x")[count],circle_position(screen, num, "y")[count],20,20),1)
-    # circles(screen)
-    red_x = [440,440,440,440,400,400,400,360,360,320]
-    red_y = [80,120,160,200,100,140,180,120,160,140]
-    green_x = [200,200,200,200,160,160,160,120,120,80]
-    green_y = [200,240,280,320,220,260,300,240,280,260]
-    blue_x = [440,440,440,440,400,400,400,360,360,320]
-    blue_y = [320,360,400,440,340,380,420,360,400,380]
+    screen.fill((255, 255, 255))
+    initialize.background(screen)  # 背景
+    initialize.draw_line(screen)  # 盤の目
+    initialize.direction(screen)
 
-    circles(screen,red_x, red_y, (249,37,0))
-    circles(screen,green_x, green_y, (64,175,78))
-    circles(screen,blue_x, blue_y, (0,0,255))
+    num = [2, 3, 10, 9, 8, 7, 8, 9, 10, 3, 2, 1]
+    for count in range(73):  # 交点の円
+        pygame.draw.rect(screen, (255, 255, 255),
+                         (rect_position(screen, num, "x")[count], rect_position(screen, num, "y")[count], 20, 20))
+        pygame.draw.rect(screen, (0, 0, 0),
+                         (rect_position(screen, num, "x")[count], rect_position(screen, num, "y")[count], 20, 20), 1)
+    # rects(screen)
+    red_x = [440, 440, 440, 440, 400, 400, 400, 360, 360, 320]
+    red_y = [80, 120, 160, 200, 100, 140, 180, 120, 160, 140]
+    green_x = [200, 200, 200, 200, 160, 160, 160, 120, 120, 80]
+    green_y = [200, 240, 280, 320, 220, 260, 300, 240, 280, 260]
+    blue_x = [440, 440, 440, 440, 400, 400, 400, 360, 360, 320]
+    blue_y = [320, 360, 400, 440, 340, 380, 420, 360, 400, 380]
+
+    initialize.rects(screen, red_x, red_y, (249, 37, 0))
+    initialize.rects(screen, green_x, green_y, (64, 175, 78))
+    initialize.rects(screen, blue_x, blue_y, (0, 0, 255))
     count = 0
-    while(1):
+    tmp = 0
+    while (1):
         pygame.display.update()
-        #イベント処理
+        # イベント処理
+        af_x = [0]*10
+        af_y = [0]*10
         for event in pygame.event.get():
-            # print(count)
             if count == 0:
                 if event.type == MOUSEBUTTONDOWN:
                     x, y = event.pos
-                    click_process(x,y, red_x, red_y)
-                    # for num in range(10):
-                    if (x > red_x[3]-20) or (x < red_x[3]+20) and (y > red_y[3]-20) or (y < red_y[3]+20):
-                        print(x, red_x[3]-20, y, red_y[3]-20)
-                        # pygame.draw.rect(screen,(255,255,0),(red_x[3]-10,red_y[3]-10,20,20))
-                        pygame.draw.rect(screen,(255,255,0),(click_process(x,y, red_x, red_y),click_process(x,y, red_x, red_y)-360 ,20,20))
-                        count += 1
-                    # font(screen,red_x[num],red_y[num])
-                    # print(x, y)
-                    # pygame.draw.rect(screen,(255,255,255),(red_x[3]-10,red_y[3]-10,20,20))
-                    # pygame.draw.rect(screen,(0,0,0),(red_x[3]-10,red_y[3]-10,20,20),1)
-
-                    # red_x[3] = x
-                    # red_y[3] = y
-                    # x -= player.get_width() / 2
-                    # y -= player.get_height() / 2
-            else:
-                # print(count)
-                if event.type == MOUSEBUTTONDOWN:
-                    x, y = event.pos
+                    click_process(x, y, red_x, red_y)
                     for num in range(10):
-                        a = 0
-                count = 0
+                        if (x > red_x[num] - 20) and (x < red_x[num] + 20) and (y > red_y[num] - 20) and (
+                                y < red_y[num] + 20):
+                            pygame.draw.rect(screen, (255, 0, 255), (red_x[num]-10,red_y[num]-10, 20, 20), 0)
+                            pygame.draw.rect(screen, (0, 0, 0), (red_x[num]-10,red_y[num]-10, 20, 20), 1)
+                            tmp = num
+                            print(num)
+                            count = 1
+                            print("a")
+            elif count == 1:
+                if event.type == MOUSEBUTTONDOWN:
+                    x,y = event.pos
+                    af_x[tmp] = initialize.mouseDownActionDirection(x, y,red_x[tmp],red_y[tmp],"typeX")
+                    af_y[tmp] = initialize.mouseDownActionDirection(x, y,red_x[tmp],red_y[tmp],"typeY")
+                    print(num,tmp,af_x[tmp], af_y[tmp],red_x[tmp], red_y[tmp])
+                    pygame.draw.rect(screen, (255, 0, 0), (af_x[tmp]+10,af_y[tmp]-10, 20, 20), 0)
+                    count = 0
 
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            #コマの表示
-            # circles(screen,red_x, red_y, (249,37,0))
-            # circles(screen,green_x, green_y, (64,175,78))
-            # circles(screen,blue_x, blue_y, (0,0,255))
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    # コマの表示
+    # initialize.rects(screen,red_x, red_y, (249,37,0))
+    # initialize.rects(screen,green_x, green_y, (64,175,78))
+    # initialize.rects(screen,blue_x, blue_y, (0,0,255))
+
 
 if __name__ == "__main__":
     main()
